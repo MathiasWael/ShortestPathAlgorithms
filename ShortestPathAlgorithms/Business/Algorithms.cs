6c92d3c12008a57888cbe4dbf19891bf7edb630c
MathiasWael <mathiaswael@hotmail.dk>
2018-08-04 13:00:26 +0200

    .

--- ShortestPathAlgorithms/Business/Algorithms.cs
+++ ShortestPathAlgorithms/Business/Algorithms.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShortestPathAlgorithms.Model
+namespace ShortestPathAlgorithms.Business
 {
-    class Block
+    class Algorithms
     {
+
     }
 }
---
+++ ShortestPathAlgorithms/Model/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPathAlgorithms.Model
+{
+    class Node
+    {
+        public enum NodeStatus { Blocked, Open, Start, End }
+        public NodeStatus Status { get; set; }
+
+        public Node()
+        {
+            Status = NodeStatus.Open;
+        }
+    }
+}
